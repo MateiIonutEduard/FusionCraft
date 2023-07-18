34243e16c9efea710026a99f2e4e7c3c27043b64
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-18 12:56:52 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FusionCraft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
